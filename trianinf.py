0c7a980a4b53c052ed1fae4162596bb706abe981
QuentinDstl <qdesautel@gmail.com>
2022-10-03 11:45:19 +0200

    trianglinf

---
+++ trianinf.py
@@ -0,0 +1,14 @@
+from numpy import zeros,array
+
+def trianginf(T, b):
+    # T est la matrice triangulaire inférieure
+    # b est le vecteur second membre
+    n = len(b)
+    x = zeros(n)
+    x[1] = b[1] / T[1][1] #b1 / T11
+    for i in range(2, n):
+        S = 0
+        for k in range(1, i-1):
+            S += T[i][k] * x[k]
+        x[i] = b[i] - S / T[i][i]
+    return x
