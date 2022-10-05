79b9a31db994538a88ddf524042341d488bcd753
quentinDstl <qdesautel@gmail.com>
2022-10-05 20:14:06 +0200

    modif

--- _ludecomp.py
+++ _ludecomp.py
@@ -1,4 +1,4 @@
-from numpy import zeros,array
+from numpy import zeros,array,dot
 
 def HasZerosOnDiag(A):
     n = len(A[0])
@@ -13,25 +13,25 @@ def _ludecomp(A):
     
     n = len(A[0])
     L = zeros((n,n))
-    U = zeros((n,n,n)) 
+    U = zeros((n,n)) 
     
     for j in range(n):
         L[j][j] = 1
-        U[1][0][j] = A[0][j]
+        U[0][j] = A[0][j]
         for i in range(n):
-            U[0][i][j] = A[i][j]
+            U[i][j] = A[i][j]
     
-    for d in range(1,n):
-        for i in range(n):
-            for k in range(i+1,n):
-                mu = U[d-1][k][i]/U[d-1][i][i]
-                L[k][i] = mu
-                U[d][k][0] = 0
-                for j in range(1,n):
-                    U[d][k][j] = U[d-1][k][j] - mu*U[d-1][i][j]
-    
-    print(A)
-    print(L)
-    print(U)
+    for i in range(n):
+        for k in range(i+1,n):
+            mu = U[k][i]/U[i][i]
+            L[k][i] = mu
+            U[k][0] = 0
+            for j in range(1,n):
+                U[k][j] = U[k][j] - mu*U[i][j]
+    return [L,U]
 
-_ludecomp(array([[1,2,3],[4,5,6],[7,8,9]]))
+[L,U] = _ludecomp(array([[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]]))
+
+print(L)
+print(U)
+print(dot(L,U))
